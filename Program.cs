52266200179b482368ef1fc90ad1bd693da56113
Eduariag <uariagsmart@gmail.com>
2022-12-02 15:51:38 +0300

    2 задачи семнара1 С#

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// Определить из 2-х чисел является ли 1-е число квадратом 2-го числа
+
+/*
+Console.Write("Input a firt number:  ");
+int num1=Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number:  ");
+int num2=Convert.ToInt32(Console.ReadLine());
+
+int quad=num2*num2;
+
+if(num1==quad)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+*/
+
+
+// Задача программа показывающая от введенного числа до такого же только с плюсом или минусом все числа
+
+/*
+Console.Write("Input an integre number: ");
+int num=Convert.ToInt32(Console.ReadLine());
+int current = -num;
+while(current <= num)
+{
+    Console.Write(current + " ");
+    current++;
+}
+*/
